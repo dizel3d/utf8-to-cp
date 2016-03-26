c27ed916bff2cefa573b9b3bbfcce83b013ec667
sarychev <dizel3d@gmail.com>
2016-03-26 09:55:36 +0300

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# utf8-to-cp
+Redirect UTF8 stdin to stdout in current Windows OS encoding for non-Unicode programs.
---
+++ utf8-to-cp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Utf8ToCp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.Default;
+
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
